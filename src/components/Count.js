0d50a928bddea7acf06acb809914a4abc19aef60
evans <techlacollege@gmail.com>
2023-07-18 23:35:20 +0300

    completion of counter

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 
 function Count() {
-    const {count, setcount} =useState(0)
+    const [count, setcount] =useState(0)
   return (
     
     <>
-    <h1>Count: {setcount}</h1>
+    <h1>Count: {count}</h1>
+    <button onClick={()=>setcount(count +1)}>increase</button>
+    <button onClick={()=>setcount(count - count)}>reset</button>
+    <button onClick={()=>setcount(count -1)}>decrease</button>
     </>
   )
 }
