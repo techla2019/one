545798ba7568a459993132fb2d50e20d2093ff14
evans <techlacollege@gmail.com>
2023-07-18 22:51:39 +0300

    final touch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <>
       <h1>hello world</h1>
+      <h2>real trial and a success!!! heeew</h2>
+      </>
+      
     </div>
   );
 }
